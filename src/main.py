de1756fcc5b6874f2ace91e2d067fff11c350cfe
yudaxx <yudax42@gmail.com>
2021-01-14 13:33:51 +0100

    getting video duration done

--- src/main.py
+++ src/main.py
@@ -2,28 +2,42 @@ import sys, getopt
 import ffmpeg
 
 #inputs
-inputfile = ''
-day = ''
-
 
 #Parsing the arguments
 def parseArgs(argv):
     try:
         opts, args = getopt.getopt(argv,"i:d:")
     except getopt.GetoptError:
-        print 'main.py -i <inputfile> -d <day>'
+        print('Usage: main.py -i <inputfile> -d <day>')
         sys.exit(2)
     if len(opts) != 2:
+        print('Usage: main.py -i <inputfile> -d <day>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-i':
             inputfile = arg
         elif opt == '-d':
             day = arg
+    return inputfile, day
+
+#Video processing
+def videoInf(inputfile):
+    meta = ffmpeg.probe(inputfile)
+    duration = round(float(meta['streams'][0]['duration']) / 60)
+    print(duration)
+
+def process(inputfile, day):
+    print("processing...", inputfile, day)
+    videoInf(inputfile)
+    #stream = ffmpeg.input(inputfile)
+    #audio = stream.audio.filter('-an')
+    #stream = ffmpeg.output(audio,stream,'twitter1.mp4')
+    #ffmpeg.run(stream)
 
 
 def main(argv):
-    parseArgs(argv)
+    inputfile,day = parseArgs(argv)
+    process(inputfile,day)
 
 
 if __name__ == "__main__":
